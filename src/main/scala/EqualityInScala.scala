df85a85224738372e545ab08b571b5318e105e18
Manish Mishra <manish.mishra@knoldus.com>
2023-04-14 22:24:29 +0530

    Added loggers

--- src/main/scala/EqualityInScala.scala
+++ src/main/scala/EqualityInScala.scala
@@ -1,37 +1,46 @@
+import com.typesafe.scalalogging.Logger
+
 class EqualityInScala[T] {
 
+  private val loggers = Logger(getClass)
+
   /* This method is used to check the list with .equals() method */
-  def equalsCheck(firstList: List[T], secondList: List[T]): (Boolean, Boolean) = {
+  def equalsCheck(firstList: T, secondList: T): (Boolean, Boolean) = {
+    loggers.info(" Checking inside the equal to method ")
     val checkWithSameList = firstList.equals(firstList)
     val checkWithDifferentList = firstList.equals(secondList)
     (checkWithSameList, checkWithDifferentList)
   }
 
   /* This method is used to check the list with == operator */
-  def equalsEqualsCheck(firstList: List[T], secondList: List[T]): (Boolean, Boolean) = {
+  def equalsEqualsCheck(firstList: T, secondList: T): (Boolean, Boolean) = {
+    loggers.info(" Checking inside the == to operator ")
     val checkWithSameList = firstList == firstList
     val checkWithDifferentList = firstList == secondList
     (checkWithSameList, checkWithDifferentList)
   }
 
   /* This method is used to check the list with != operator*/
-  def notEqualsCheck(firstList: List[T], secondList: List[T]): (Boolean, Boolean) = {
-    val checkWithSameList = firstList != secondList
+  def notEqualsCheck(firstList: T, secondList: T): (Boolean, Boolean) = {
+    loggers.info(" Checking inside the != to operator ")
+    val checkWithSameList = firstList != firstList
     val checkWithDifferentList = firstList != secondList
     (checkWithSameList, checkWithDifferentList)
   }
 
   /* This method is used to check the list with ne keyword
      ne applied on same instance then it gives false else it give true */
-  def neCheck(firstList: List[T], secondList: List[T]): (Boolean, Boolean) = {
+  def neCheck[A](firstList: List[A], secondList: List[A]): (Boolean, Boolean) = {
+    loggers.info(" Checking inside the ne to operator ")
     val checkWithSameList = firstList ne firstList
     val checkWithDifferentList = firstList ne secondList
     (checkWithSameList, checkWithDifferentList)
   }
 
-  /* This method is used to check the list with ne keyword
+  /* This method is used to check the list with eq keyword
      eq applied on same instance then it gives true else it give false */
-  def eqCheck(firstList: List[T], secondList: List[T]): (Boolean, Boolean) = {
+  def eqCheck[A](firstList: List[A], secondList: List[A]): (Boolean, Boolean) = {
+    loggers.info(" Checking inside the eq to operator ")
     val checkWithSameList = firstList eq firstList
     val checkWithDifferentList = firstList eq secondList
     (checkWithSameList, checkWithDifferentList)
