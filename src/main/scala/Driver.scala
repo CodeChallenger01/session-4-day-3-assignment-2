bccd8a6e2e78911c3157f7246b05eb2bb0b02197
Manish Mishra <manish.mishra@knoldus.com>
2023-04-14 22:23:56 +0530

    checked for other type as well

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,9 +1,13 @@
+import com.typesafe.scalalogging.Logger
+
 import scala.util.{Failure, Success, Try}
 
 object Driver extends App {
+  private val loggers = Logger(getClass)
   private val firstList: List[Int] = List(1, 2, 3, 4, 5)
   private val secondList: List[Int] = List(1, 2, 3, 4, 5)
-  private val obj = new EqualityInScala[Int]
+  loggers.info("Checking with List Type")
+  private val obj = new EqualityInScala[List[Int]]
   private val result = {
     Try {
       val equalsResult = obj.equalsCheck(firstList, secondList)
@@ -15,7 +19,49 @@ object Driver extends App {
     }
   }
   result match {
-    case Success(value) => println(value)
-    case Failure(exception) => exception.getMessage
+    case Success(value) => loggers.info(s"$value")
+    case Failure(exception) => loggers.info(s"${exception.getMessage}")
+  }
+
+  loggers.info("Checking with String Type")
+  private val string = new EqualityInScala[String]
+  val firstString = "Manish"
+  val secondString = "manish"
+  private val listOfString: List[String] = List("Manish", "Ravi")
+  private val listOfOtherString: List[String] = List("Manish", "Ravi")
+  private val stringResult = {
+    Try {
+      val equalsResult = string.equalsCheck(firstString, secondString)
+      val equalsEqualsResult = string.equalsEqualsCheck(firstString, secondString)
+      val notEqualsResult = string.notEqualsCheck(firstString, secondString)
+      val neResult = string.neCheck(listOfString, listOfOtherString)
+      val eqResult = string.eqCheck(listOfString, listOfOtherString)
+      (equalsResult, equalsEqualsResult, notEqualsResult, neResult, eqResult)
+    }
+  }
+  stringResult match {
+    case Success(value) => loggers.info(s"$value")
+    case Failure(exception) => loggers.info(s"${exception.getMessage}")
+  }
+
+  loggers.info("Checking with Integer Type")
+  private val integer = new EqualityInScala[Int]
+  private val firstInteger = 14
+  private val secondInteger = 15
+  private val listOfInteger: List[Int] = List(16, 18, 20)
+  private val listOfOtherInteger: List[Int] = List(12, 17, 19)
+  private val integerResult = {
+    Try {
+      val equalsResult = integer.equalsCheck(firstInteger, secondInteger)
+      val equalsEqualsResult = integer.equalsEqualsCheck(firstInteger, secondInteger)
+      val notEqualsResult = integer.notEqualsCheck(firstInteger, secondInteger)
+      val neResult = integer.neCheck(listOfInteger, listOfOtherInteger)
+      val eqResult = integer.eqCheck(listOfInteger, listOfOtherInteger)
+      (equalsResult, equalsEqualsResult, notEqualsResult, neResult, eqResult)
+    }
+  }
+  integerResult match {
+    case Success(value) => loggers.info(s"$value")
+    case Failure(exception) => loggers.info(s"${exception.getMessage}")
   }
 }
